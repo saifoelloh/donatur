685498bb468880e6ce43893a4f507371ecc4a3d6
saifoelloh <saifoelloh@gmail.com>
2019-06-23 01:57:36 +0700

    membuat migrasi untuk enpoint donatur

---
+++ database/migrations/2019_06_22_150831_create_donaturs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDonatursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donaturs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama');
+            $table->string('email');
+            $table->string('alamat');
+            $table->enum('status', ['tetap', 'tidak tetap']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donaturs');
+    }
+}
