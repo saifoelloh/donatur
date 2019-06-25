88b76ec5199b4eb55405f74230c074c1f556e3a5
saifoelloh <saifoelloh@gmail.com>
2019-06-26 00:43:27 +0700

    menyelesaikan integrasi antara sistem dan tampilan dari buat anak asuh

--- resources/views/vendor/adminlte/admin/anak-asuh/index.blade.php
+++ resources/views/vendor/adminlte/admin/anak-asuh/index.blade.php
@@ -30,6 +30,7 @@
                             <th class="text-center">Nama</th>
                             <th class="text-center">No Telp</th>
                             <th class="text-center">Alamat</th>
+                            <th class="text-center">Pendidikan</th>
                             <th class="text-center">Gender</th>
                             <th class="text-center">Tindakan</th>
                         </tr>
@@ -47,19 +48,55 @@
             $('.data-table').dataTable({
                 data: {!! $data !!},
                 columns: [
-                    {data: 'id'},
-                    {data: 'nik'},
-                    {data: 'nama'},
-                    {data: 'no_telp'},
-                    {data: 'alamat'},
-                    {data: 'gender'},
+                    {
+                        data: 'id',
+                        render: function(data) {
+                            return '<p class="text-center p-0 m-0">'+data+'</p>';
+                        }
+                    },
+                    {
+                        data: 'nik',
+                        render: function(data) {
+                            return '<p class="text-center p-0 m-0">'+data+'</p>';
+                        }
+                    },
+                    {
+                        data: 'nama',
+                        render: function(data) {
+                            return '<p class="text-center p-0 m-0">'+data+'</p>';
+                        }
+                    },
+                    {
+                        data: 'no_telp',
+                        render: function(data) {
+                            return '<p class="text-center p-0 m-0">'+data+'</p>';
+                        }
+                    },
+                    {
+                        data: 'alamat',
+                        render: function(data) {
+                            return '<p class="text-center p-0 m-0">'+data+'</p>';
+                        }
+                    },
+                    {
+                        data: 'pendidikan',
+                        render: function(data) {
+                            return '<p class="text-center p-0 m-0">'+data+'</p>';
+                        }
+                    },
+                    {
+                        data: 'gender',
+                        render: function(data) {
+                            return '<p class="text-center p-0 m-0">'+data+'</p>';
+                        }
+                    },
                     {
                         data: 'id',
                         render: function(data) {
                             const link = 'http://localhost:8000/admin/anak-asuh/'+data;
-                            const detail = '<a class="btn btn-primary btn-xs" style="margin: 0 3px" href="'+link+'">edit</a>';
+                            const edit = '<a class="btn btn-primary btn-xs" style="margin: 0 3px" href="'+link+'">edit</a>';
                             const hapus = '<form role="form" action="'+link+'" style="margin: 0 3px;display: inline" method="POST">@method("DELETE")@csrf<button class="btn btn-danger btn-xs">delete</button></form>';
-                            return '<div class="text-center">'+detail+hapus+'</div>';
+                            return '<div class="text-center">'+edit+hapus+'</div>';
                         }
                     },
                 ]
