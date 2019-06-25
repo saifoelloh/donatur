a5563ea5f881ecd1bd6677afdd2e9cacb7be7f45
saifoelloh <saifoelloh@gmail.com>
2019-06-25 16:47:38 +0700

    membuat route untuk anak asuh

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,18 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::group([
     'prefix' => 'admin',
-    'as' => 'admin.'
 ], function() {
     Route::get('/dashboard', 'HomeController@index')->name('dashboard');
     Route::group([
         'prefix' => 'anak-asuh',
         'as' => 'anak-asuh'
     ], function () {
-        Route::get('/', function () {
-            return view('welcome');
-        });
+        Route::get('/', 'AnakAsuhController@index');
+        Route::post('/', 'AnakAsuhController@store')->name('.store');
+        Route::get('/create', 'AnakAsuhController@create')->name('.create');
+        Route::get('/{id}', 'AnakAsuhController@edit')->name('.edit');
+        Route::put('/{id}', 'AnakAsuhController@update')->name('.update');
+        Route::delete('/{id}', 'AnakAsuhController@destroy')->name('.destroy');
     });
     Route::group([
         'prefix' => 'donatur',
