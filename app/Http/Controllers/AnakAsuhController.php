8d4e9f93ac5a713b9111e306712d0ffc7b8432ea
saifoelloh <saifoelloh@gmail.com>
2019-06-26 00:48:44 +0700

    integrasi antara controller dengan tampilan

--- app/Http/Controllers/AnakAsuhController.php
+++ app/Http/Controllers/AnakAsuhController.php
@@ -39,7 +39,29 @@ class AnakAsuhController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $findAnak = AnakAsuh::where('nik', $request->nik)->get();
+        if (sizeof($findAnak)!=0) {
+            return abort(400);
+        }
+
+        try {
+            AnakAsuh::create([
+                'nik' => $request->nik,
+                'nama' => $request->nama,
+                'alamat' => $request->alamat,
+                'no_telp' => $request->no_telp,
+                'gender' => $request->gender,
+                'tempat_lahir' => $request->tempat_lahir,
+                'tanggal_lahir' => $request->tanggal_lahir,
+                'pendidikan' => $request->pendidikan,
+                'status' => $request->status,
+                'tanggal_masuk' => $request->tanggal_masuk,
+            ]);
+        } catch (\Throwable $th) {
+            return $th;
+        }
+
+        return redirect()->route('anak-asuh');
     }
 
     /**
@@ -61,7 +83,11 @@ class AnakAsuhController extends Controller
      */
     public function edit($id)
     {
-        //
+        $findAnak = AnakAsuh::find($id);
+
+        return view('vendor.adminlte.admin.anak-asuh.edit', [
+            'anak' => $findAnak,
+        ]);
     }
 
     /**
@@ -73,7 +99,29 @@ class AnakAsuhController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $findAnak = AnakAsuh::find($id);
+        if ($findAnak==null) {
+            return abort(404);
+        }
+
+        try {
+            $findAnak->update([
+                'nik' => $request->nik,
+                'nama' => $request->nama,
+                'alamat' => $request->alamat,
+                'no_telp' => $request->no_telp,
+                'gender' => $request->gender,
+                'tempat_lahir' => $request->tempat_lahir,
+                'tanggal_lahir' => $request->tanggal_lahir,
+                'pendidikan' => $request->pendidikan,
+                'status' => $request->status,
+                'tanggal_masuk' => $request->tanggal_masuk,
+            ]);
+        } catch (\Throwable $th) {
+            return abort(500,$th);
+        }
+
+        return redirect()->route('anak-asuh');
     }
 
     /**
@@ -84,6 +132,12 @@ class AnakAsuhController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try {
+            AnakAsuh::destroy($id);
+        } catch (\Throwable $th) {
+            return abort(500, $th);
+        }
+
+        return redirect()->route('anak-asuh');
     }
 }
