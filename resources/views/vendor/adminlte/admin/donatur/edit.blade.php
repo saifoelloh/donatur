d73e751a4ce883d6d48578e69ead8c6c0a1ffbda
saifoelloh <saifoelloh@gmail.com>
2019-06-27 00:43:22 +0700

    menambahkan list donasi kedalam halaman edit donatur

--- resources/views/vendor/adminlte/admin/donatur/edit.blade.php
+++ resources/views/vendor/adminlte/admin/donatur/edit.blade.php
@@ -9,7 +9,7 @@
     </div>
     <div class="col-md-6 text-right">
         <h3>
-            <a class="btn btn-info" href={{route('donatur')}}>
+            <a class="btn btn-info" href="{{route('donatur')}}">
                 Kembali
             </a>
         </h3>
@@ -53,4 +53,72 @@
         </div>
     </div>
 </div>
+<div class="box box-success">
+    <div class="box-header">
+        <h3 class="box-title">
+            List Donasi
+        </h3>
+        <h3 class="box-title pull-right">
+            <a class="btn btn-info" href="{{route('donasi.create')}}">
+                Tambah Donasi
+            </a>
+        </h3>
+    </div>
+    <div class="box-body">
+        <div class="row">
+            <div class="col-md-12">
+                <table class="data-table table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th class="text-center">Id</th>
+                            <th class="text-center">Tanggal</th>
+                            <th class="text-center">Jumlah</th>
+                            <th class="text-center">Tindakan</th>
+                        </tr>
+                    <thead>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
+
+@section('js')
+    <script>
+        $(document).ready(function () {
+            $('.data-table').dataTable({
+                data: {!! $donasis !!},
+                columns: [
+                    {
+                        data: 'id',
+                        render: function(data) {
+                            return '<p class="text-center p-0 m-0">'+data+'</p>';
+                        }
+                    },
+                    {
+                        data: 'tanggal',
+                        render: function(data) {
+                            return '<p class="text-center p-0 m-0">'+data+'</p>';
+                        }
+                    },
+                    {
+                        data: 'jumlah',
+                        render: function(data) {
+                            const result = 'Rp. '+data;
+                            return '<p class="text-center p-0 m-0">'+result+'</p>';
+                        }
+                    },
+                    {
+                        data: 'id',
+                        render: function(data) {
+                            const link = 'http://localhost:8000/admin/donasi/'+data;
+                            const detail = '<a class="btn btn-primary btn-xs" style="margin: 0 3px" href="'+link+'">detail</a>';
+                            const hapus = '<form role="form" action="'+link+'" style="margin: 0 3px;display: inline" method="POST">@method("DELETE")@csrf<button class="btn btn-danger btn-xs">delete</button></form>';
+                            return '<div class="text-center">'+detail+hapus+'</div>';
+                        }
+                    },
+                ]
+            });
+        });
+    </script>
 @stop
